199ec3a2c13c0b5bd0144f8b15ebd3144b41bf6e
fatia1 <130059555+MeErickss@users.noreply.github.com>
2024-11-08 10:41:42 -0300

    justificar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,11 +7,7 @@ function App() {
 
   return (
     <div 
-      className="
-        min-h-screen w-screen
-        bg-cinza-600 text-cinza-100 antialiased
-      "
-    >
+      className="min-h-screen w-screen bg-cinza-600 text-cinza-100 antialiased">
       <header className="flex flex-1 justify-center items-center py-20 px-3 bg-neutral-900">
         <img src="/logo.svg" alt="" />
       </header>
@@ -20,26 +16,9 @@ function App() {
         
         <div className="flex justify-between items-center gap-2 -translate-y-2/4">
           <input 
-            type="text"
-            className="
-            bg-cinza-500 text-cinza-100
-              border-[1px] border-cinza-700 rounded-md
-              h-12 p-4 leading-normal outline-none
-              w-full
-
-            "  
-          />
+            type="text"className="bg-cinza-500 text-cinza-100 border-[1px] border-cinza-700 rounded-md h-12 p-4 leading-normal outline-none"/>
           <button 
-            className="
-              flex justify-center items-center
-              h-12 p-4
-              bg-azul-dark text-cinza-100
-              hover:bg-azul
-              border-0 rounded-md
-              font-bold text-sm
-              transition-all
-            "
-          >
+            className=" flex justify-center items-center h-12 p-4 bg-azul-dark text-cinza-100 hover:bg-azul border-0 rounded-md font-bold text-sm transition-all">
             Criar
           </button>
         </div>
@@ -70,10 +49,8 @@ function App() {
                 <label htmlFor="checkbox" className="flex items-center gap-3 p-1">
                   <input type="checkbox" className="hidden"/>
                   <span 
-                    className="
-                      rounded-full w-4 h-4 flex items-center justify-center 
-                      border-2 border-blue-500 hover:border-blue-300"
-                  >
+                    className="rounded-full w-4 h-4 flex items-center justify-center border-2 border-blue-500 hover:border-blue-300"
+>
                     {/* Icone */}
                   </span>
                   <p>
